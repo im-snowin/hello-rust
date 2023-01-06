0d3277e14d47f60238d1089690441e603ac85958
im-snowin <hello.snowin@gmail.com>
2023-01-06 20:26:16 +0530

    struct in rust

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: i8,
+    email: String
+}
+
+
 fn main() {
-    
-    let mut years: [i32; 3] = [1920, 1947, 2002];
 
-    println!("{:?}", years);
-    
-    years[2] = 1988;
-    
-    println!("{:?}", years);
+    let person1: Person= Person{
+        name: String::from("snowin"),
+        age: 18,
+        email: String::from("snowin@gmail.com"),
+    };
 
-    for year in years.iter() {
-        println!("Next year: {}", year + 1);
-    }
+    // Let us print the person1
+    println!("Name: {}", person1.name);
+    println!("Age: {}", person1.age);
+    println!("Email: {}\n", person1.email);
 
-    let names: [&str; 4] = ["snowin", "mark", "kalo", "john"];
-    println!("{:?}", names)
+    // Let us destruct the preson
+    let Person {name, age, email} = person1;
+
+    println!("Name: {}", name);
+    println!("Age: {}", age);
+    println!("Email: {}\n", email);
+
+    // lets mut struct
+    let mut person2 = Person{
+        name: String::from("mark"),
+        age: 23,
+        email: String::from("mark@gmail.com"),
+    };
+
+    person2.name = String::from("Mark Henry");
+
+    println!("{:#?}", person2)
+
 }
