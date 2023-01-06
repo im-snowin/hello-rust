ece87a12f2675884373297ba0d5616521702b68f
im-snowin <hello.snowin@gmail.com>
2023-01-06 19:07:23 +0530

    say hello to functions 👋

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
+
+
 fn main() {
-    /* Scalar Types */
-    let age:i8 = 18;
-    let cgp:f32 = 7.5;
+  say_hello(String::from("snowin"));
+}
 
-    let is_active:bool = true;
-
-    let initial:char = 'J';
-
-    let name:&str = "snowin";
-
-    println!("{} {} is {} years old with the CGP of {}. Active:{}.", name, initial, age, cgp, is_active);
-
-    // compound data types
-    let names:[&str; 3] = ["snowin", "mark", "kevin"];
-    println!("Names: {:?}", names);   
+fn say_hello(name: String){
+    println!("hello, {}!", name);
 }
